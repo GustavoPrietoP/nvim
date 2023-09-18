0a39d29f69a8c558eb5d88f0f038dc6dcb600cac
GustavoPrietoP <vgdvx17@gmail.com>
2023-09-18 18:22:01 -0400

    updated nvim config

---
+++ lua/core/keys.lua
@@ -0,0 +1,51 @@
+vim.g.mapleader = ' '
+local map = vim.api.nvim_set_keymap
+
+-- Moving Between Buffers
+map('n', '<C-h>', '<C-w>h', { noremap = true, silent = false })
+map('n', '<C-l>', '<C-w>l', { noremap = true, silent = false })
+map('n', '<C-j>', '<C-w>j', { noremap = true, silent = false })
+map('n', '<C-k>', '<C-w>k', { noremap = true, silent = false })
+
+map('n', '<Leader>oe', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+map('n', '<C-f>', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
+map('n', '<C-\\>', ':ToggleTerm<CR>', { noremap = true, silent = true })
+
+map('n', 'zR', ':lua require("ufo").openAllFolds<CR>', { noremap = true, silent = true })
+map('n', 'zM', ':lua require("ufo").closeAllFolds<CR>', { noremap = true, silent = true })
+
+map("n", "K", "<cmd>Lspsaga hover_doc<CR>", { noremap = true, silent = true })
+
+map('n', '<Esc>', ':nohl<CR>', { silent = true })
+
+map('n', '<leader>bc', ':bdelete<CR>', { silent = true })
+
+map('i', 'jk', '<ESC>', { silent = true })
+
+map('v', '<', '<gv', { noremap = true, silent = true })
+map('v', '>', '>gv', { noremap = true, silent = true })
+
+map('x', 'K', ':move \'<-2<CR>gv-gv', { noremap = true, silent = true })
+map('x', 'J', ':move \'>+1<CR>gv-gv', { noremap = true, silent = true })
+
+vim.cmd([[
+  nnoremap <silent> <C-Up>    :resize -2<CR>
+  nnoremap <silent> <C-Down>  :resize +2<CR>
+  nnoremap <silent> <C-Right>  :vertical resize -2<CR>
+  nnoremap <silent> <C-Left>  :vertical resize +2<CR>
+]])
+
+-- disable ex mode
+vim.cmd('nnoremap Q <Nop>')
+
+-- disable recording
+vim.cmd('nnoremap q <Nop>')
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
---
+++ lua/plugs/util/which-key.lua
@@ -0,0 +1,55 @@
+local wk = require("which-key")
+wk.setup {
+  triggers = { "<leader>" }
+}
+
+local mappings = {
+  q = {
+    name = 'file',
+    q = { ":q<cr>", "quit" },
+    w = { ":wq<cr>", "save & quit" },
+    s = { ":w<cr>", "save" },
+    f = { ":lua vim.lsp.buf.format()<cr>", "format file" },
+    x = { ":bdelete<cr>", "close" },
+  },
+  f = {
+    name = 'Telescope',
+    f = { ":Telescope find_files<cr>", "find files" },
+    r = { ":Telescope oldfiles<cr>", "recently opened" },
+    w = { ":Telescope live_grep<cr>", "find string" },
+    c = { ":lua require('prism.picker').open()<cr>", "kolorschemes" },
+  },
+  g = {
+    name = "grapple",
+    a = { ":grappletag<cr>", "add a bookmark" },
+    t = { ":grappletoggle<cr>", "toggle bookmark" },
+    r = { ":grappleuntag<cr>", "remove bookmark" },
+    g = { ":grapplepopup tags<cr>", "show all bookmarks" }
+  },
+  t = {
+    name = "terminal",
+    t = { ":ToggleTerm<cr>", "split below" },
+  },
+  l = {
+    name = "misc",
+    l = { ":lazy<cr>", "open lazy" },
+    c = { ":lua require('telescope') vim.lsp.buf.code_action()<cr>", "show code actions" },
+    m = { ":mason<cr>", "open mason" },
+    s = { ":symbolsoutline<cr>", "overview of file" },
+    w = { ":sessionsave<cr>", "save this session" }
+  },
+  h = {
+    name = "hop",
+    c = { ":hopchar1<cr>", "single character" },
+    c = { ":hopchar2<cr>", "double characters" },
+    a = { ":hopanywhere<cr>", "anywhere" },
+    l = { ":hopline<cr>", "line" },
+    l = { ":hoplinestart<cr>", "line start" },
+    v = { ":hopvertical<cr>", "vertically" },
+    w = { ":hopword<cr>", "word" },
+
+  }
+}
+
+local opts = { prefix = '<leader>' }
+wk.register(mappings, opts)
